820124668123c574f8cc5a13617d391a477a5364
Guilherme Rigotti <ggrigotti@gmail.com>
2023-03-29 15:56:22 -0300

    🎉 feat(coupon.rb, create_coupons.rb, schema.rb): add Coupon model and migration A new Coupon model has been added to the application with validations for code, status, due_date, and discount_value. The status attribute is an enum with two possible values: active and inactive. A migration has been created to add a new coupons table to the database with columns for code, status, discount_value, and due_date. The schema.rb file has been updated to reflect the new coupons table.
    ✨ feat(coupons.rb, coupon_spec.rb): add coupon factory and model tests
    The coupon factory has been added to generate coupon objects for testing purposes. The coupon model tests have been added to validate the presence, uniqueness, and numericality of the coupon code, status, due date, and discount value. The coupon status is now an enum with two possible values: active and inactive.

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -12,12 +12,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_329_142_708) do
+ActiveRecord::Schema[7.0].define(version: 20_230_329_184_618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
   create_table 'categories', force: :cascade do |t|
     t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+  create_table 'coupons', force: :cascade do |t|
+    t.string 'code'
+    t.integer 'status'
+    t.decimal 'discount_value', precision: 5, scale: 2
+    t.datetime 'due_date'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
