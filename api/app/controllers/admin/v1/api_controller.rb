5a1db7e61832a8629eec6abc6a4e4660bd70255b
Guilherme Rigotti <ggrigotti@gmail.com>
2023-03-30 19:30:27 -0300

    🚀 feat(api_controller.rb): add before_action to restrict access for admin 🐛 fix(api_controller.rb): fix syntax error in render_error method 🐛 fix(api_controller.rb): fix syntax error in render method The `restrict_access_for_admin!` method is added as a `before_action` to restrict access for admin. The `ForbiddenAccess` exception is rescued and an error message is rendered. Syntax errors in the `render_error` and `render` methods are fixed.

--- api/app/controllers/admin/v1/api_controller.rb
+++ api/app/controllers/admin/v1/api_controller.rb
@@ -6,12 +6,24 @@ module Admin
       class ForbiddenAccess < StandardError; end
       include Authenticate
 
+      before_action :restrict_access_for_admin!
+
       def render_error(message: nil, fields: nil, status: :unprocessable_entity)
         errors = {}
         errors['fields'] = fields if fields.present?
         errors['message'] = message if message.present?
 
         render json: { errors: }, status:
+      end
+
+      rescue_from ForbiddenAccess do
+        render_error(message: 'Forbidden access', status: :forbidden)
+      end
+
+      private
+
+      def restrict_access_for_admin!
+        raise ForbiddenAccess unless current_user.admin?
       end
     end
   end
