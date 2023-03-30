9794fc79734cfc3bcd8a35bd3cb991060d1cd5c9
Guilherme Rigotti <ggrigotti@gmail.com>
2023-03-30 14:06:17 -0300

    🚀 feat(product): add image attachment and validation The Product model now has a one-to-one relationship with an image attachment. The image is required and validated using the `validate_presence_of` method in the product_spec.rb file. A product_image.png file has been added to the support/images directory for testing purposes. The product factory has been updated to include an image attachment when creating a new product.

--- api/spec/factories/products.rb
+++ api/spec/factories/products.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     sequence(:name) { |n| "Basic #{n}" }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 1..400) }
+    image { Rack::Test::UploadedFile.new(Rails.root.join('spec/support/images/product_image.png')) }
 
     after :build do |product|
       product.productable = create(:game)
