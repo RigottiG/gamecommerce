fa8ced7932d8b6910287922a2bb5adacea9c8236
Guilherme Rigotti <ggrigotti@gmail.com>
2023-03-30 15:06:39 -0300

    🐛 fix(api_controller.rb): fix syntax error in render_error method ✨ feat(categories_controller.rb): add create and save_category! methods, and category_params private method. Add show.json.jbuilder view. The render_error method in api_controller.rb had a syntax error, which has been fixed. In categories_controller.rb, the create method has been added, which creates a new category and saves it using the save_category! method. The category_params method is used to permit the name parameter. The save_category! method saves the category and renders the show view. If there is an error, it calls the render_error method with the category errors. The show.json.jbuilder view has been added to render the category id and name.
    🚀 feat(categories_spec.rb): add tests for POST /categories endpoint
    This commit adds tests for the POST /categories endpoint. The tests cover both valid and invalid attributes. When valid attributes are provided, a new category is created, and the last created category is returned with a success status. When invalid attributes are provided, no new category is created, an error message is returned, and an unprocessable entity status is returned.

--- api/app/controllers/admin/v1/api_controller.rb
+++ api/app/controllers/admin/v1/api_controller.rb
@@ -4,6 +4,14 @@ module Admin
   module V1
     class ApiController < ApplicationController
       include Authenticable
+
+      def render_error(message: nil, fields: nil, status: :unprocessable_entity)
+        errors = {}
+        errors['fields'] = fields if fields.present?
+        errors['message'] = message if message.present?
+
+        render json: { errors: }, status:
+      end
     end
   end
 end
--- api/spec/requests/admin/v1/categories_spec.rb
+++ api/spec/requests/admin/v1/categories_spec.rb
@@ -19,4 +19,50 @@ RSpec.describe 'Admin::V1::Categories', type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  context 'POST /categories' do
+    let(:url) { '/admin/v1/categories' }
+
+    context 'with valid attributes' do
+      let(:attributes) { { category: attributes_for(:category) }.to_json }
+
+      it 'creates a new category' do
+        expect do
+          post url, headers: auth_header(user), params: attributes
+        end.to change(Category, :count).by(1)
+      end
+
+      it 'returns last created category' do
+        post url, headers: auth_header(user), params: attributes
+        expect_category = Category.last.as_json(only: %i[id name])
+
+        expect(body_json['category']).to eq(expect_category)
+      end
+
+      it 'returns success status' do
+        post url, headers: auth_header(user), params: attributes
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'with invalid attributes' do
+      let(:attributes) { { category: attributes_for(:category, name: nil) }.to_json }
+
+      it "doesn't create a new category" do
+        expect do
+          post url, headers: auth_header(user), params: attributes
+        end.to_not change(Category, :count)
+      end
+
+      it 'returns error message' do
+        post url, headers: auth_header(user), params: attributes
+        expect(body_json['errors']['fields']).to have_key('name')
+      end
+
+      it 'returns unprocessable entity status' do
+        post url, headers: auth_header(user), params: attributes
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
 end
